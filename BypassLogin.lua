e21f402c7b8eb368ae3f8167a5e8938e6ef2c710
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-03-08 11:27:03 -0600

    Created BypassLogin.lua
    Added version 1.

---
+++ BypassLogin.lua
@@ -0,0 +1,15 @@
+-- Bypass basic Roblox GUI login
+-- by: Celvis#5477
+
+-- Please note that you may have to change somethings up yourself to get it to work. Just depends. If you are smart enough you'll figure it out.
+
+-- variables
+local textBox = game:GetService("CoreGui").Loader.LoaderFrame.KeyFrame.TextBox;
+local old; old = hookmetamethod(textBox, "__index", function(self, index)
+    
+    -- check
+    if index == "Text" then
+        return debug.getstack(3)[12][3];
+    end
+    return old(self, index);
+end);
